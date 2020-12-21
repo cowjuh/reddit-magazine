23b9147d08bd52855c3b7858ec445d0af10984e6
cowjuh <jenzhang530@gmail.com>
2020-12-20 20:18:04 -0700

    Add proper pagination

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,29 +3,33 @@ import {FaArrowRight, FaArrowLeft, FaRedoAlt} from 'react-icons/fa'
 import {Link, useLocation} from 'react-router-dom'
 import InputField from './InputField'
 
-export default function Navigation() {
+export default function Navigation(props) {
+    const curPage = props.curPage
+    const handlePageTurn = props.handlePageTurn
     const location = useLocation()
     const path = location.pathname
-    var current = parseInt(path.slice(path.length - 1))
-    var prev
-    var next
-    current === 1? prev = 1: prev = current - 1
-    current === 9? next = 1: next = current + 1
-    
-    function handleSubmit(id, subReddit) {
-        console.log(subReddit)
+    var prev, next
+    if(curPage === 1){
+        prev = 20
+    } else {
+        prev = curPage - 1
+    }
+    if(curPage === 20){
+        next = 1
+    } else {
+        next = curPage + 1
     }
 
     return(
         <div className='flex-row'>
-            <Link exact to={`/${prev}`}>
+            <Link exact to={`/${prev}`} onClick={() => handlePageTurn(prev)}>
                 <FaArrowLeft color='black'/>
             </Link>
-            <p>{current}</p>
-            <Link exact to={`/${next}`}>
+            <p>{curPage}</p>
+            <Link exact to={`/${next}`} onClick={() => handlePageTurn(next)}>
                 <FaArrowRight color='black'/>
             </Link>
-            <Link exact to={`/1`}>
+            <Link exact to={`/1`} onClick={() => handlePageTurn(1)}>
                 <FaRedoAlt color='black'/>
             </Link>
         </div>
